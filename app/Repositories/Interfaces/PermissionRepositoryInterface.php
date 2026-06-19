9efa1925cff803df0c3df8ac9fc199eae9458566
mannan608 <mannanswe608@gmail.com>
2026-06-19 14:55:15 +0600

    refactor: update 5 php files
    - 5 files changed: 1 insertions(+), 337 deletions(-)

--- app/Repositories/Interfaces/PermissionRepositoryInterface.php
+++ app/Repositories/Interfaces/PermissionRepositoryInterface.php
@@ -7,15 +7,9 @@ use Spatie\Permission\Models\Permission;
 
 interface PermissionRepositoryInterface
 {
-    public function allGroupedByFeature(?string $search = null): Collection;
+    public function allGroupedByFeature(): Collection;
 
     public function findByNames(array $names): Collection;
 
     public function firstOrCreate(string $name, ?string $guardName = null): Permission;
-
-    public function create(array $data): Permission;
-
-    public function update(Permission $permission, array $data): Permission;
-
-    public function delete(Permission $permission): bool;
 }
